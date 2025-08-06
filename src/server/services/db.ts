1d9becf76ad62d0f2eb667b7a61d10a2e15d4d8f
Joaquim d'Souza <joaquimds@gmail.com>
2025-08-06 18:17:59 +0200

    fix: correct db SSL flag

--- src/server/services/db.ts
+++ src/server/services/db.ts
@@ -2,7 +2,7 @@ import { Pool } from "pg";
 
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL,
-  ssl: true
+  ssl: process.env.POSTGRES_URL?.includes(".")
 });
 
 export default pool;
